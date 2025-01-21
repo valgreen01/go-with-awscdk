15d3339e39c7f51d56741fc9a1c82f81ec3e0166
Aldo Valverde <alvane.01@gmail.com>
2025-01-20 20:50:58 -0600

    Initial creation of lambda with cdk

--- go_cdk.go
+++ go_cdk.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
+
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -20,6 +22,12 @@ func NewGoCdkStack(scope constructs.Construct, id string, props *GoCdkStackProps
 
 	// The code that defines your stack goes here
 
+	awslambda.NewFunction(stack, jsii.String("myLambdaFunction"), &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
+		Handler: jsii.String("main"),
+	})
+
 	// example resource
 	// queue := awssqs.NewQueue(stack, jsii.String("GoCdkQueue"), &awssqs.QueueProps{
 	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
@@ -29,7 +37,7 @@ func NewGoCdkStack(scope constructs.Construct, id string, props *GoCdkStackProps
 }
 
 func main() {
-	defer jsii.Close()
+	defer jsii.Close() // run jsii shutdown logic at the end of the program
 
 	app := awscdk.NewApp(nil)
 
